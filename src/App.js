9cad14341b20ce4cafec94bec530af4500b990ca
Yeji Jang <yejijang95@gmail.com>
2024-03-19 18:02:43 -0400

    added sentences data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import lines from "./data.js";
+import React, { useState, useEffect } from "react";
+
+const setNewSentence = () => {
+  const idx = Math.floor(Math.random() * lines.length);
+  console.log(lines[idx]);
+  return lines[idx];
+};
 
 function App() {
+  const [sentence, setSentence] = useState(setNewSentence());
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>{sentence}</div>
     </div>
   );
 }
